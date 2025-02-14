a40236a98b492f2dd6b8f17ab3e1f8df2e45d084
taimoor-th1904453 <taimoor.hussain@fau.de>
2025-02-14 07:32:11 +0100

    bugs

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    extends: [
+      // The official Next.js + Core Web Vitals recommended rules:
+      'next',
+      'next/core-web-vitals'
+    ],
+    rules: {
+      // add or override any rules here
+    },
+  }
+  
--- quran-eye-tracker/next.config.mjs
+++ quran-eye-tracker/next.config.mjs
@@ -1,6 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  // other config if needed
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Provide a false fallback for fs to silence the build error
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
